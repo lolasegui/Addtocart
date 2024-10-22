39bcc289cccc0fecb3f794a1f494ef0d4efd74dd
lolasegui <lsegui19939@iesjoanramis.org>
2024-10-22 12:45:54 +0200

    Add files via upload

--- ADDTOCART/js/script.js
+++ ADDTOCART/js/script.js
@@ -1,8 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const input = document.getElementById("inputField");
-const lola = document.getElementById("afegir")
+const boto = document.getElementById("afegir");
 const lista = document.getElementById("llista");
 
 const appSettings = {
@@ -13,8 +13,27 @@ const baseDades = getDatabase(app);
 const tasks = ref(baseDades, "tareas");
 
 
-lola.addEventListener("click", function() {
+boto.addEventListener("click", function() {
     push(tasks, input.value)
-    lista.innerHTML += `<li>${input.value}</li>`;
-    input.value = ""
+    
+    addElement(input);
+    clearScreen();
+
 })
+
+function addElement(e){
+    lista.innerHTML += `<li>${e}</li>`;
+}
+
+function clearScreen(){
+    input.value= ""
+}
+
+onValue(tasks, function (snapshot) {
+    let resultats =Object.values(snapshot.val())
+    for (let i=0; i < resultats.length; i++){
+        let current= resultats[i]
+        addElement(current)
+    }
+})
+
